b7e97727e6eadb1f033059175d96eacb4d6b5ad8
ArtemZaikovsky <c-12artem@yandex.ru>
2021-06-27 19:07:21 +0300

    CrudSecurity

---
+++ src/main/java/web/controller/AdminController.java
@@ -0,0 +1,65 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import web.model.Role;
+import web.model.User;
+import web.service.UserService;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private UserService userService;
+
+    @GetMapping()
+    public String getUsers(ModelMap model) {
+        model.addAttribute("users", userService.getAllUser());
+        return "users";
+    }
+
+    @GetMapping("/{id}")
+    public String getUser(@PathVariable("id") int id, ModelMap model) {
+        model.addAttribute("user", userService.getUser(id));
+        return "user";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "new";
+    }
+
+    @PostMapping()
+    public String addUser(@ModelAttribute("user") User user) {
+        Role role = new Role(2L, "USER");
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        user.setRoles(roles);
+        userService.addUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(ModelMap modelMap, @PathVariable("id") int id) {
+        modelMap.addAttribute("user", userService.getUser(id));
+        return "edit";
+    }
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userService.updateUser(id, user);
+        return "redirect:/admin";
+    }
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+}
